2e484734f1180cfd5b8c319bf264a1d50ba19b8d
bashir <bashirahmed.cse@gmail.com>
2023-01-24 22:47:04 +0600

    update

--- resources/views/product_js.blade.php
+++ resources/views/product_js.blade.php
@@ -1,5 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+<script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
 <script>
             $.ajaxSetup({
         headers: {
@@ -24,6 +25,27 @@
                 $('#addProductModal').modal('hide');
                 $('#addProductFrom')[0].reset();
                 $('.table').load(location.href+ ' .table');
+
+                Command: toastr["success"]("Product Added!", "success")
+
+                    toastr.options = {
+                    "closeButton": true,
+                    "debug": false,
+                    "newestOnTop": false,
+                    "progressBar": true,
+                    "positionClass": "toast-top-right",
+                    "preventDuplicates": false,
+                    "onclick": null,
+                    "showDuration": "300",
+                    "hideDuration": "1000",
+                    "timeOut": "5000",
+                    "extendedTimeOut": "1000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                    }
+
                }
               },error:function(err){
               let error = err.responseJSON;
@@ -62,6 +84,27 @@
                 $('#updateProductModal').modal('hide');
                 $('#updateProductFrom')[0].reset();
                 $('.table').load(location.href+ ' .table');
+
+                Command: toastr["success"]("Product Updated!", "success")
+
+                    toastr.options = {
+                    "closeButton": true,
+                    "debug": false,
+                    "newestOnTop": false,
+                    "progressBar": true,
+                    "positionClass": "toast-top-right",
+                    "preventDuplicates": false,
+                    "onclick": null,
+                    "showDuration": "300",
+                    "hideDuration": "1000",
+                    "timeOut": "5000",
+                    "extendedTimeOut": "1000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                    }
+
                }
               },error:function(err){
               let error = err.responseJSON;
@@ -84,6 +127,26 @@
               success:function(res){
                if(res.status=='success'){
                 $('.table').load(location.href+ ' .table');
+
+                Command: toastr["success"]("Product Deleted!", "success")
+
+                    toastr.options = {
+                    "closeButton": true,
+                    "debug": false,
+                    "newestOnTop": false,
+                    "progressBar": true,
+                    "positionClass": "toast-top-right",
+                    "preventDuplicates": false,
+                    "onclick": null,
+                    "showDuration": "300",
+                    "hideDuration": "1000",
+                    "timeOut": "5000",
+                    "extendedTimeOut": "1000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                    }
                }
               }
             });
